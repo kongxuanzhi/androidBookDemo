eaeec020ee2e806ed8434e871f0cb67026f6f3ce
kongxuanzhi <875248646@qq.com>
2015-12-11 22:19:33 +0800

    android入门书本Demo

---
+++ studyDemo/gen/com/xuan/studydemo/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.xuan.studydemo;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ studyDemo/src/com/xuan/Dialog/alertDialog.java
@@ -0,0 +1,12 @@
+package com.xuan.Dialog;
+
+import android.content.Context;
+import android.support.v7.app.AlertDialog;
+
+public class alertDialog extends AlertDialog {
+
+	public alertDialog(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+}
